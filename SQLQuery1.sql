23fbe80f9bd2f72ec2e8d696f454ce9df45b1fb1
Genildon Barreto <genildonbarreto@yahoo.com.br>
2025-06-21 21:27:31 -0300

    Adicionadas novas consultas SQL para buscar informações sobre atores e filmes

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -35,3 +35,53 @@ WHERE Duracao > 100 AND Duracao < 150
 ORDER BY Duracao
 
 --7- Buscar a quantidade de filmes lançadas no ano, agrupando por ano, ordenando pela duracao em ordem decrescente
+SELECT
+	Ano,
+	COUNT(*) Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC
+
+-- 8- Buscar os Atores do gênero masculino, retornando o PrimeiroNome, UltimoNome
+SELECT
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores 
+WHERE Genero = 'M'
+
+-- 9- Buscar os Atores do gênero feminino, retornando o PrimeiroNome, UltimoNome, e ordenando pelo PrimeiroNome
+SELECT
+	PrimeiroNome,
+	UltimoNome,
+	Genero
+FROM Atores 
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome
+
+-- 10- Buscar o nome do filme e o gênero
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM FilmesGenero
+INNER JOIN Filmes ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+
+-- 11- Buscar o nome do filme e o gênero do tipo "Mistério"
+SELECT
+	Filmes.Nome,
+	Generos.Genero
+FROM FilmesGenero
+INNER JOIN Filmes ON Filmes.Id = FilmesGenero.IdFilme
+INNER JOIN Generos ON FilmesGenero.IdGenero = Generos.Id
+WHERE Generos.Genero = 'MISTÉRIO'
+
+-- 12- Buscar o nome do filme e os atores, trazendo o PrimeiroNome, UltimoNome e seu Papel
+SELECT
+	Filmes.Nome,
+	Atores.PrimeiroNome,
+	Atores.UltimoNome,
+	ElencoFilme.Papel
+FROM Filmes
+INNER JOIN ElencoFilme ON ElencoFilme.IdFilme = Filmes.Id
+INNER JOIN Atores ON Atores.Id = ElencoFilme.IdAtor
